4a70ba5143a2bb2317b2970637d92e02cd09c2b6
LeoMbm <leonidas.j.mbm@gmail.com>
2023-07-31 14:40:53 +0200

    show prompt, add ctrl+d exit

--- main.c
+++ main.c
@@ -2,16 +2,27 @@
 
 
 int main(int ac, char **argv){
-    char *prompt = "XXX $";
+    char *prompt = "XXX $ ";
     char *line;
     size_t len = 0;
+    ssize_t chars_read;
 
     (void)ac; (void)argv;
+    // Create a loop that prints the prompt and waits for input
+    while (1){    
+        printf("%s", prompt);
+        chars_read = getline(&line, &len, stdin);
+        // Check for EOF (Ctrl+D or failure)
+        if (chars_read == -1){
+            printf("Exiting...\n");
+            return -1;
+        }   
+        printf("%s\n", line);
+        free(line);
+    }
 
-    printf("%s", prompt);
-    getline(&line, &len, stdin);
-    printf("%s\n", line);
 
-    free(line);
+
+
     return 0;
 }
